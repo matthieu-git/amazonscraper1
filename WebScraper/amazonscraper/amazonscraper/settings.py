38286aeb73ed8af208c0b437b84c6d77a43c8f3f
matthieu-git <35268345+matthieu-git@users.noreply.github.com>
2019-11-19 20:40:29 -0500

    update

--- WebScraper/amazonscraper/amazonscraper/settings.py
+++ WebScraper/amazonscraper/amazonscraper/settings.py
@@ -22,6 +22,9 @@ USER_AGENT = 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
 
+# Enable proxy pool
+# PROXY_POOL_ENABLED = True
+
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
 
@@ -50,6 +53,14 @@ ROBOTSTXT_OBEY = True
 #SPIDER_MIDDLEWARES = {
 #    'amazonscraper.middlewares.AmazonscraperSpiderMiddleware': 543,
 #}
+
+
+# DOWNLOADER_MIDDLEWARES = {
+#     # ...
+#     'scrapy_proxy_pool.middlewares.ProxyPoolMiddleware': 610,
+#     'scrapy_proxy_pool.middlewares.BanDetectionMiddleware': 620,
+#     # ...
+# }
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
--- WebScraper/amazonscraper/amazonscraper/spiders/amazon_spider.py
+++ WebScraper/amazonscraper/amazonscraper/spiders/amazon_spider.py
@@ -4,18 +4,22 @@ from ..items import AmazonscraperItem
 
 class AmazonSpiderSpider(scrapy.Spider):
     name = 'amazon'
-    start_urls = ['https://www.amazon.ca/s?k=avocado&qid=1574118995&ref=sr_pg_1']
+    start_urls = ['https://www.amazon.ca/s?k=avocado&page=2&qid=1574200266&ref=sr_pg_2']
 
     def parse(self, response):
 
         items = AmazonscraperItem()
 
-        prod_name = response.css('.widgetId\=search-results > .s-border-bottom .a-color-base.a-text-normal').css('::text').extract()
-        prod_price_whole = response.css('.a-price-whole::text').extract()
-        prod_price_frac = response.css('.a-price-fraction::text').extract()
+        all_prods = response.css('.s-include-content-margin')
 
-        items['prod_name'] = prod_name
-        items['prod_price_whole'] = prod_price_whole
-        items['prod_price_frac'] = prod_price_frac
+        for avo in all_prods:
 
-        yield items
+            prod_name = avo.css('.widgetId\=search-results > .s-border-bottom .a-color-base.a-text-normal').css('::text').extract()
+            prod_price_whole = avo.css('.product-list__listview-price::text').extract()
+            prod_price_frac = avo.css('.product-list__listview-price::text').extract()
+
+            items['prod_name'] = prod_name
+            items['prod_price_whole'] = prod_price_whole
+            items['prod_price_frac'] = prod_price_frac
+
+            yield items
